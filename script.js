3ca1185a16160838a07285eb6cfcd26bc7607e8f
Nicolas David <nicolasdavid9.nd@gmail.com>
2023-01-18 20:04:26 +0100

    probleme injection input > titre.html

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 //label
 const searchInput = document.getElementById('searchbar');
 //input
-const titleCity = document.getElementById('city-input');
-//btn
+const searchCity = document.getElementById('city-input');
+//btn 
 const searchBtn = document.getElementById('btn');
 //Affichage de la ville 
 const cityTitle = document.getElementById('title-city');
@@ -17,15 +17,19 @@ const humidite = document.getElementById('humidity');
 const pression = document.getElementById('pressure');
 
 //**************** RECUPERATION API  ******************/
-const meteo = fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&lang=fr&units=metric&appid=e0a85717b253d52d725887cc3f83c45f`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&lang=fr&units=metric&appid=e0a85717b253d52d725887cc3f83c45f`)
 .then((res) => res.json()) // Formatage de la réponse en format JSON (format objet)
 .then((data) => {
-    cityTitle.innerHTML = data.name;
+    if(searchCity.value === undefined){
+        cityTitle.innerHTML = ""
+    }else{
+        cityTitle.innerHTML = searchCity.value
+    };
     description.innerHTML = "description : " + data.weather[0].description;
     temperature.innerHTML = "température : " + data.main.temp + " °C";
-    vent.innerHTML = "vent : " + data.wind.speed + ' km/h';
+    vent.innerHTML = "vent : " + data.wind.speed + ' m/s';
     humidite.innerHTML = "humidité : " + data.main.humidity + ' %';
-    pressure.innerHTML = " pression atmosphérique :" + data.main.pressure + " hPa";
+    pression.innerHTML = " pression atmosphérique :" + data.main.pressure + " hPa";
     console.log(data);
 })
 .catch((err) => console.error(err)) // Si erreur 
